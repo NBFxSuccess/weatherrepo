30ae49e598836af088751110f39625d3c5a9a1f7
K4ne <nick@getmighty.net>
2022-04-10 23:30:17 -0500

    refactored js code-shortened using for loops

--- assets/script.js
+++ assets/script.js
@@ -27,7 +27,6 @@ let thirdIcon = document.getElementById("thirdicon");
 let fourthIcon = document.getElementById("fourthicon");
 let fifthIcon = document.getElementById("fifthicon");
 let cityname = document.getElementById("cityname");
-
 let searchBtn = document.getElementById("search")
 let temperature = document.getElementById("temperature")
 let humidity = document.getElementById("humidity")
@@ -35,11 +34,11 @@ let wind = document.getElementById("wind")
 let uvIndex = document.getElementById("uvindex")
 let currentDate = moment().format('l')
 const now = moment();
-firstDayEl.innerText = now.add(1, 'days').format('l');
-secondDayEl.innerText = now.add(1, 'days').format('l');
-thirdDayEl.innerText = now.add(1, 'days').format('l');
-fourthDayEl.innerText = now.add(1, 'days').format('l');
-fifthDayEl.innerText = now.add(1, 'days').format('l');
+firstDayEl.innerText = now.add(1, 'days').format('dddd');
+secondDayEl.innerText = now.add(1, 'days').format('dddd');
+thirdDayEl.innerText = now.add(1, 'days').format('dddd');
+fourthDayEl.innerText = now.add(1, 'days').format('dddd');
+fifthDayEl.innerText = now.add(1, 'days').format('dddd');
 searchBtn.addEventListener("click",searchbtn)
 currentIcon.hidden = true;
 firstIcon.hidden = true;
@@ -64,11 +63,10 @@ function searchbtn() {
 return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityData.city.coord.lat}&lon=${cityData.city.coord.lon}&appid=${apikey}`);
 })
 .then(response => response.json())
-.then(forecastData =>{
+.then(forecastData =>
+{
     let currentIconCode = `${forecastData.current.weather[0].icon}`;
     let currentIconUrl = `http://openweathermap.org/img/wn/${currentIconCode}@2x.png`;
-    let firstIconCode = `${forecastData.current.weather[0].icon}`;
-    let firstIconCodeUrl = `http://openweathermap.org/img/wn/${currentIconCode}@2x.png`;
     currentIcon.setAttribute("src",currentIconUrl)
     currentIcon.hidden = false;
     firstIcon.hidden = false;
@@ -78,36 +76,19 @@ return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityData.cit
     fifthIcon.hidden = false;
     console.log(forecastData)
     
-    let firstDayCode = `${forecastData.daily[1].weather[0].icon}`;
-    let firstDayUrl = `http://openweathermap.org/img/wn/${firstDayCode}@2x.png`;
-    let secondDayCode = `${forecastData.daily[2].weather[0].icon}`;
-    let secondDayUrl = `http://openweathermap.org/img/wn/${secondDayCode}@2x.png`;
-    let thirdDayCode = `${forecastData.daily[3].weather[0].icon}`;
-    let thirdDayUrl = `http://openweathermap.org/img/wn/${thirdDayCode}@2x.png`;
-    let fourthDayCode = `${forecastData.daily[4].weather[0].icon}`;
-    let fourthDayUrl = `http://openweathermap.org/img/wn/${fourthDayCode}@2x.png`;
-    let fifthDayCode = `${forecastData.daily[5].weather[0].icon}`;
-    let fifthDayUrl = `http://openweathermap.org/img/wn/${fifthDayCode}@2x.png`;
-    firstIcon.setAttribute("src",firstDayUrl)
-    secondIcon.setAttribute("src",secondDayUrl)
-    thirdIcon.setAttribute("src",thirdDayUrl)
-    fourthIcon.setAttribute("src",fourthDayUrl)
-    fifthIcon.setAttribute("src",fifthDayUrl)
-    firstHumidityEl.innerHTML = "Humidity:" + forecastData.daily[1].humidity + "%";
-    secondHumidityEl.innerHTML = "Humidity:" + forecastData.daily[2].humidity + "%";
-    thirdHumidityEl.innerHTML = "Humidity:" + forecastData.daily[3].humidity + "%";
-    fourthHumidityEl.innerHTML = "Humidity:" + forecastData.daily[4].humidity + "%";
-    fifthHumidityEl.innerHTML = "Humidity:" + forecastData.daily[5].humidity + "%";
-    firstWindEl.innerHTML = "Wind:" + forecastData.daily[1].wind_speed + "MPH"
-    secondWindEl.innerHTML = "Wind:" + forecastData.daily[2].wind_speed + "MPH"
-    thirdWindEl.innerHTML = "Wind:" + forecastData.daily[3].wind_speed + "MPH"
-    fourthWindEl.innerHTML = "Wind:" + forecastData.daily[4].wind_speed + "MPH"
-    fifthWindEl.innerHTML = "Wind:" + forecastData.daily[5].wind_speed + "MPH"
-    firstDayTempEl.innerHTML = "Temp:" + Math.round((1.8*(forecastData.daily[1].temp.day - 273) + 32) * 100)/100 + "°F";
-    secondDayTempEl.innerHTML = "Temp:" + Math.round((1.8*(forecastData.daily[2].temp.day - 273) + 32) * 100)/100 + "°F";
-    thirdDayTempEl.innerHTML = "Temp:" + Math.round((1.8*(forecastData.daily[3].temp.day - 273) + 32) * 100)/100 + "°F";
-    fourthDayTempEl.innerHTML = "Temp:" + Math.round((1.8*(forecastData.daily[4].temp.day - 273) + 32) * 100)/100 + "°F";
-    fifthDayTempEl.innerHTML = "Temp:" + Math.round((1.8*(forecastData.daily[3].temp.day - 273) + 32) * 100)/100 + "°F";
+    const icons = ["",firstIcon,secondIcon,thirdIcon,fourthIcon,fifthIcon];
+    const humudities = ["",firstHumidityEl,secondHumidityEl,thirdHumidityEl,fourthHumidityEl,fifthHumidityEl];
+    const winds = ["",firstWindEl,secondWindEl,thirdWindEl,fourthWindEl,fifthWindEl]
+    const temps = ["",firstDayTempEl,secondDayTempEl,thirdDayTempEl,fourthDayTempEl,fifthDayTempEl]
+
+    for (let i = 1; i < 6; i++)
+    {
+        icons[i].setAttribute("src", `http://openweathermap.org/img/wn/${forecastData.daily[i].weather[0].icon}@2x.png`)
+        winds[i].innerHTML = "Wind:" + forecastData.daily[i].wind_speed + "MPH"
+        humudities[i].innerHTML = "Humidity:" + forecastData.daily[i].humidity + "%";
+        temps[i].innerHTML = "Temp:" + Math.round((1.8*(forecastData.daily[i].temp.day - 273) + 32) * 100)/100 + "°F";
+    }
+
     temperature.innerHTML = "Temp: " + Math.round((1.8*(forecastData.current.temp - 273) + 32) * 100)/100 + "°F";
     humidity.innerHTML = "Humidity: " + forecastData.current.humidity + "%";
     uvIndex.innerHTML = "UV Index: " + forecastData.current.uvi;
